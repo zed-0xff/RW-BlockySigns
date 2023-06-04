650896294e49f59f304d29e1985f29f4ec60e45b
Andrey "Zed" Zaikin <zed.0xff@gmail.com>
2023-06-04 18:39:13 +0300

    fix

--- Source/Patch_Building_Storage.cs
+++ Source/Patch_Building_Storage.cs
@@ -1,16 +1,18 @@
 using HarmonyLib;
 using RimWorld;
+using Verse;
 
 namespace Blocky.Signs;
 
 static class Patch_Building_Storage {
     [HarmonyPatch(typeof(Building_Storage), nameof(Building_Storage.Accepts))]
     static class FrameAcceptSingleItem {
-        static void Postfix(ref bool __result, Building_Storage __instance){
+        static void Postfix(ref bool __result, Building_Storage __instance, Thing t){
             if( __result == false ) return;
             if( !(__instance is Building_Frame f) ) return;
 
-            __result = !f.Occupied;
+            __result = !f.Occupied ||
+                t.Position == __instance.Position; // already in
         }
     }
 }
